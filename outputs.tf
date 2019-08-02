d358ff7efeeb1feefe3837c50f1aa04e79277329
Sander van Harmelen <sander@vanharmelen.nl>
2019-08-02 21:25:41 +0200

    The quotes aren't needed, but lots of tools expect them

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output id {
+output "id" {
   value       = aws_directory_service_directory.default.id
   description = "The directory identifier"
 }
 
-output access_url {
+output "access_url" {
   value       = aws_directory_service_directory.default.access_url
   description = "The access URL for the directory"
 }
 
-output dns_ip_addresses {
+output "dns_ip_addresses" {
   value       = aws_directory_service_directory.default.dns_ip_addresses
   description = "A list of IP addresses of the DNS servers for the directory"
 }
 
-output security_group_id {
+output "security_group_id" {
   value       = aws_directory_service_directory.default.security_group_id
   description = "The ID of the security group created by the directory"
 }
